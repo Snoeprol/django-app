88c4316cee3bd5fe1643ec13e75202daae79da25
Snoeprol <mariovanrooij@hotmail.com>
2022-08-24 10:53:37 +0200

    added placeholder for score and date

--- scores/forms.py
+++ scores/forms.py
@@ -5,7 +5,10 @@ class EditUserForm(forms.ModelForm):
   class Meta:
     model = Score
     fields = ["date","score"]
-    
+    widgets = {
+            'date': forms.TextInput(attrs={'placeholder': 'Date (YYYY-MM-DD)'}),
+            'score': forms.TextInput(attrs={'placeholder': 'Score'}),
+              }
 class DateRange(forms.Form):
   # Date 1
   date1 = forms.DateField(label="Starting Date", widget=forms.SelectDateWidget(years=range(2000 ,2025)))
