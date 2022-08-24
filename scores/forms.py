a1ab9f75ab396653557b8820249e5ab698ff4de8
Snoeprol <mariovanrooij@hotmail.com>
2022-08-24 10:55:10 +0200

    last cleanup

--- scores/forms.py
+++ scores/forms.py
@@ -9,7 +9,7 @@ class EditUserForm(forms.ModelForm):
             'date': forms.TextInput(attrs={'placeholder': 'Date (YYYY-MM-DD)'}),
             'score': forms.TextInput(attrs={'placeholder': 'Score'}),
               }
+    
 class DateRange(forms.Form):
-  # Date 1
   date1 = forms.DateField(label="Starting Date", widget=forms.SelectDateWidget(years=range(2000 ,2025)))
   date2 = forms.DateField(label="Ending Date", widget=forms.SelectDateWidget(years=range(2000,2025)))
--- scores/urls.py
+++ scores/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.home, name='main-home'),
     path('auth/', views.auth, name='main-auth'),
     path('edit/', views.edit_members, name='main-edit-members'),
-   # path('user_page/', views.user_page, name='main-user-page'),
     re_path(r'^user/(?P<user>[-\w]*)/$', views.user_page, name='main-user-page')
 
 ]
